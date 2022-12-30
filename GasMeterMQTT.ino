252b21574459e2a7091853f2b172cfdb4604f371
Tobias Stöger <34764984+tstoegi@users.noreply.github.com>
2022-12-30 11:56:31 +0100

    Initial commit

---
+++ GasMeterMQTT.ino
@@ -0,0 +1,295 @@
+/***************************************************************************
+  Gas Meter Sensor (LJ12A3-4-Z/BX 5V) via mqtt
+  + Wemos D1 Mini
+  + Voltage divider: (Sensor BK)->5V---R10K---A0---R22K---GND
+
+  2022 @tstoegi
+  Tobias Stöger
+ ***************************************************************************/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+
+#include <Credentials.h>
+/* Credentials.h should look like
+  #define mySSID "yourSSID"
+  #define myPASSWORD "1234567890"
+*/
+
+WiFiClientSecure espClient;
+PubSubClient mqttClient(espClient);
+char msg[50];
+
+//mqtt_server
+const char* mqtt_server = myMQTTBroker_Server;
+const unsigned int mqtt_port = myMQTTBroker_Port;
+
+const char* mqtt_fprint = myMQTTBroker_Cert_FPrint;
+
+//mqtt_user
+const char* mqtt_user = myMQTTBroker_User_ESP_gasmeter;
+const char* mqtt_pass = myMQTTBroker_Pass_ESP_gasmeter;
+
+//mqtt_topic
+String mqttTopic_out = "haus/gasmeter";
+String clientId = "gasmeter_";
+
+const float m3_oneround = 0.01;  // one complete round 0.01m3 (or 10 liter)
+float m3_total = 0.0;            // total since start
+
+#define SIGNAL_SENSOR A0
+#define SIGNAL_HYST_MIN 450
+#define SIGNAL_HYST_MAX 550
+#define SIGNAL_CHECK_EVERY_SEC 1
+
+
+enum State {
+  state_startup = 0,
+  state_setupWifi = 1,
+  state_setupOTA = 2,
+  state_setupMqtt = 3,
+  state_checkSensorData = 4,
+  state_sendMqtt = 5
+} nextState;
+
+int nextStateDelaySeconds = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("\n Gas Meter Monitoring");
+
+  nextState = state_startup;
+  nextStateDelaySeconds = 0;
+}
+
+void loop() {
+  if (nextState > state_setupOTA) {
+    ArduinoOTA.handle();
+  }
+
+  static unsigned long prevMillis = 0;
+  while (millis() - prevMillis >= nextStateDelaySeconds * 1000) {
+    prevMillis = millis();
+    doNextState(nextState);
+  }
+  delay(1000);
+}
+void setNextState(State aNextState, int aNextStateDelaySeconds = 0) {
+  nextState = aNextState;
+  nextStateDelaySeconds = aNextStateDelaySeconds;
+}
+void doNextState(State aNewState) {
+  Serial.print("___ doNextState() ___: ");
+  Serial.println(aNewState);
+
+  switch (aNewState) {
+    case state_startup:
+      {
+        Serial.println("state_startup");
+
+        pinMode(LED_BUILTIN, OUTPUT);
+
+        setNextState(state_setupWifi);
+        break;
+      }
+    case state_setupWifi:
+      {
+        Serial.println("state_setupWifi");
+        setupWifi();
+        setNextState(state_setupOTA);
+        break;
+      }
+    case state_setupOTA:
+      {
+        Serial.println("state_setupOTA");
+        setupOTA();
+        setNextState(state_setupMqtt);
+        break;
+      }
+    case state_setupMqtt:
+      {
+        Serial.println("state_setupMqtt");
+        setupMqtt();
+        setNextState(state_checkSensorData);
+        break;
+      }
+    case state_checkSensorData:
+      {
+        Serial.println("state_checkSensorData");
+        int adc = analogRead(SIGNAL_SENSOR);
+        Serial.println(adc);
+
+        digitalWrite(LED_BUILTIN, false);
+        delay(50);
+        digitalWrite(LED_BUILTIN, true);
+
+        static bool active = false;
+        if (!active && adc < SIGNAL_HYST_MIN) {
+          active = true;
+        }
+        Serial.println(active ? "Sensor is on" : "Sensor is off");
+
+        if (active && adc > SIGNAL_HYST_MAX) {
+          active = false;
+          Serial.println(active ? "Sensor is on" : "Sensor is off");
+          setNextState(state_sendMqtt);
+          break;  // done
+        }
+
+        digitalWrite(LED_BUILTIN, !active);
+
+        setNextState(state_checkSensorData, SIGNAL_CHECK_EVERY_SEC);
+        break;
+      }
+    case state_sendMqtt:
+      {
+        Serial.println("state_sendMqtt");
+        m3_total += m3_oneround;
+        mqttPublish(String(m3_oneround), "m3used");
+        mqttPublish(String(m3_total), "m3total");
+
+        static unsigned long lastMillis = millis();
+        unsigned long duration = millis() - lastMillis;
+        lastMillis = millis();
+        mqttPublish(String(duration), "durationInMillis");
+
+        setNextState(state_checkSensorData);
+        break;
+      }
+    default:
+      {
+        Serial.println("!!! unknown status !!!");
+      }
+  }
+
+  if (aNewState > state_setupMqtt) {
+    if (!mqttClient.loop()) {  //needs to be called regularly
+      mqttReconnect();
+    }
+
+    for (int i = 0; i < 100; i++) {  // todo Hack - if more incoming messages are queued
+      mqttClient.loop();
+    }
+  }
+
+
+  delay(100);
+}
+void mqttPublish(String msg, const char* subTopic) {
+  Serial.print(">> Published message: ");
+  String topicString = mqttTopic_out + "/" + subTopic;
+  Serial.print(topicString);
+  Serial.print(" ");
+  Serial.println(msg);
+  mqttClient.publish(topicString.c_str(), msg.c_str(), true);  //We send with "retain"
+}
+
+void setupWifi() {
+  WiFi.forceSleepWake();
+  WiFi.mode(WIFI_STA);  // <<< Station
+  delay(300);
+  // Wait for connection
+  int retries = 5;
+  while (WiFi.status() != WL_CONNECTED && retries > 0) {
+    WiFi.begin(mySSID, myPASSWORD);
+    Serial.print(retries);
+    delay(1000);
+    int secondsTimeout = 10;
+    while (WiFi.status() != WL_CONNECTED && secondsTimeout > 0) {
+      delay(1000);
+      Serial.print(".");
+      secondsTimeout--;
+    }
+    retries--;
+    Serial.println();
+  }
+
+  if (retries == 0) {
+    Serial.println(mySSID);
+    Serial.println("Connection failed!");
+    return;
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(mySSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupOTA() {
+  String clientID = clientId + WiFi.localIP().toString();
+  Serial.println(clientID);
+  ArduinoOTA.setHostname(clientID.c_str());
+
+  ArduinoOTA.setPassword("esp8266");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+}
+
+void setupMqtt() {
+  // Setting insecure disables the fingerprint verification.
+  //espClient.setInsecure 3();
+  if (strlen(mqtt_fprint) > 0) {
+    Serial.println("Setting mqtt server fingerprint");
+    espClient.setFingerprint(mqtt_fprint);
+  }
+  mqttClient.setServer(mqtt_server, mqtt_port);
+  //mqttClient.setCallback(mqttCallback);
+}
+
+void mqttReconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    // Create a random client ID
+    clientId += WiFi.localIP().toString();  // mqtt clientId must be unique !!!
+
+    // Attempt to connect
+    if (mqttClient.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+
+      // mqttClient.subscribe(mqttTopicOut.c_str());
+      // Serial.println(mqttTopicOut.c_str());
+
+      mqttClient.loop();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
