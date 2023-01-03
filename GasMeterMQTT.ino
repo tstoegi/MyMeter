3253a349e6630fc91502bb01b2fb9e280887f6e4
Tobias Stöger <34764984+tstoegi@users.noreply.github.com>
2023-01-03 10:19:42 +0100

    Fix: Value 0 after around 500 cycles
    Used sizeof a struct wrong. So the end of the EEPROM storage was calculated wrong.

--- GasMeterMQTT.ino
+++ GasMeterMQTT.ino
@@ -101,6 +101,9 @@ struct GasCounter {
 };
 GasCounter gasCounter = { 0.00f };  // todo set initial value via mqtt subscribe
 
+// we store the new value in EEPROM always at currentEEPROMAddress+1 to spread max write-life-cycles of the whole EEPROM - at the end we start from 0 again
+int sizeofStructGasCounter = 10;  // do not use sizeof!!! it will not work on structs
+
 int currentEEPROMAddress = 0;
 #define MAGIC_BYTE '#'
 #define EEPROM_SIZE_BYTES_MAX 512
@@ -476,6 +479,7 @@ void deepSleep() {
 void findLastEEPROMAddress() {
   Serial.println("### EEPROM DUMP ###");
   for (; currentEEPROMAddress < EEPROM_SIZE_BYTES_MAX - sizeof(gasCounter); currentEEPROMAddress++) {  //max EEPROM_SIZE_BYTES-length data(0..511 bytes)
+  for (; currentEEPROMAddress < EEPROM_SIZE_BYTES_MAX; currentEEPROMAddress++) {
     // read a byte from the current address of the EEPROM
     char value = EEPROM.read(currentEEPROMAddress);
     Serial.print(currentEEPROMAddress);
@@ -502,6 +506,7 @@ void findLastEEPROMAddress() {
 void increaseEEPROMAddress() {
   currentEEPROMAddress++;
   if (currentEEPROMAddress + 1 >= EEPROM_SIZE_BYTES_MAX - sizeof(gasCounter)) {
+  if (currentEEPROMAddress > EEPROM_SIZE_BYTES_MAX - sizeofStructGasCounter) {
     // we start from the beginning
     currentEEPROMAddress = 0;
   }
