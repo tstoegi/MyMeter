7a445deabce7b8e78418a6113fb8ee24e16293d6
Tobias Stöger <34764984+tstoegi@users.noreply.github.com>
2023-01-29 17:13:37 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
-#define CO_MYMETER_NAME "gasmeter"  // unique name of your device, e.g. gasmeter (used for mqtt client id and OTA network name)
+#define CO_MYMETER_NAME "mymeter"  // unique name of your device, e.g. gasmeter (used for mqtt client id and OTA network name)
 
 #define CO_MQTT_BROKER_IP "192.168.?.?"          // ip of your mqtt broker/server
 #define CO_MQTT_BROKER_PORT 8883                  // 8883 via SSL/TLS, 1883 plain
-#define CO_MQTT_TOPIC_MAIN_FOLDER_PUB "home/"     // mqtt main folder
+#define CO_MQTT_TOPIC_MAIN_FOLDER_PUB "home/"     // mqtt main folder for e.g. home/mymeter
 #define CO_MQTT_TOPIC_SUB_FOLDER_SUB "/settings"  // mqtt sub folder for home/mymeter/settings/config, e.g. waitForOTA, total or voltageCalibration
 
 // #define STATIC_IP  // uncomment this line, if you want to use a static IP (for faster WiFi connection)
